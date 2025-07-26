4fb0ff64c626daad34f3e4d6b7b6fa977724b318
Don Lee <git@mrdonlee.addy.io>
2025-07-26 20:45:10 +0100

    refactor: change border and padding style

--- lib/widgets/chat_input_widget.dart
+++ lib/widgets/chat_input_widget.dart
@@ -12,20 +12,20 @@ class _ChatInput extends State<ChatInput> {
   Widget build(BuildContext context) {
     return Container(
       width: 600,
-      height: 110,
-      padding: EdgeInsets.all(10),
       decoration: BoxDecoration(
-        color: Colors.grey,
-        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
       ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           TextField(
+            maxLines: null,
             decoration: InputDecoration(
               border: InputBorder.none,
               hintText: 'How can I help you today?',
-              contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+              hintStyle: TextStyle(color: Colors.grey),
+              contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
             ),
           ),
           Row(
